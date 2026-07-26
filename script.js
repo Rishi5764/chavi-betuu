3e419f4c24121f7027dcbaee7a23673624f15d6f
Rishi5764 <163666536+Rishi5764@users.noreply.github.com>
2026-07-26 13:48:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -74,3 +74,86 @@ function createHeart(){
 }
 
 setInterval(createHeart,300);
+// Music
+
+const music=document.getElementById("bgMusic");
+
+const musicBtn=document.getElementById("musicBtn");
+
+musicBtn.onclick=()=>{
+
+if(music.paused){
+
+music.play();
+
+musicBtn.innerHTML="⏸ Pause";
+
+}else{
+
+music.pause();
+
+musicBtn.innerHTML="🎵 Music";
+
+}
+
+};
+
+// Sparkles
+
+document.addEventListener("mousemove",(e)=>{
+
+let s=document.createElement("div");
+
+s.className="sparkle";
+
+s.style.left=e.clientX+"px";
+
+s.style.top=e.clientY+"px";
+
+document.body.appendChild(s);
+
+setTimeout(()=>{
+
+s.remove();
+
+},1000);
+
+});
+
+// Fireworks
+
+function fireworks(){
+
+for(let i=0;i<30;i++){
+
+let f=document.createElement("div");
+
+f.className="firework";
+
+f.innerHTML=["🎆","✨","💖","❤️"][Math.floor(Math.random()*4)];
+
+f.style.left=Math.random()*100+"vw";
+
+f.style.top=Math.random()*100+"vh";
+
+document.body.appendChild(f);
+
+setTimeout(()=>{
+
+f.remove();
+
+},2000);
+
+}
+
+}
+
+nextBtn.onclick=()=>{
+
+letter.style.display="none";
+
+finalPage.style.display="flex";
+
+fireworks();
+
+}
